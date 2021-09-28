362796c9bdbc7c71951071b8a7506887a54bbe75
PrashantSomaniIIMI <90964968+PrashantSomaniIIMI@users.noreply.github.com>
2021-09-28 11:46:14 +0530

    Using if statement in JS

--- Script.js
+++ Script.js
@@ -1,8 +1,11 @@
 let name = prompt("what is your name")
 let age = prompt("how old are you")
-let isBoss = confirm("are you above 18 years")
 let dateofBirth = 2021-age
-alert("Hi"+ name + "." + `your age is ${age}.`+ `You were born in ${dateofBirth}`);
+if (age <18) {
+    alert("Hi"+ name + "." + `your age is ${age}.`+ `You were born in ${dateofBirth}` +
+     "you are below 18 so you cannot access the website");
+}
+else {alert("Hi"+ name + "." + `your age is ${age}.`+ `You were born in ${dateofBirth}`+"you are above 18, so access the website");}
 
 
 
