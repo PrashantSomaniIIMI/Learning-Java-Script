9a00eeef9f00317b8a9c808a48605b57cb806e30
PrashantSomaniIIMI <90964968+PrashantSomaniIIMI@users.noreply.github.com>
2021-09-28 14:02:31 +0530

    using !(not) for cheking age within a certain range

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 let name = prompt("what is your name")
 let age = prompt("What is your age")
-if (age>=18 && age<=100) {
-    alert(`Hi ${name}, you can try this game`)
-    } else {alert("no you cant")}
+if (!(age>=18 && age<=100)) {
+    alert(`Hi ${name}, no you cant try this game`)
+    } else {alert("You are Welcome")}
    
