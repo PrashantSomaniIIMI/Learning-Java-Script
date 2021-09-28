905cf853468a14febeaf8352585874f473831a48
PrashantSomaniIIMI <90964968+PrashantSomaniIIMI@users.noreply.github.com>
2021-09-28 12:26:43 +0530

    Using If Else statement

--- Script.js
+++ Script.js
@@ -1,18 +1,7 @@
 let name = prompt("what is your name")
-let age = prompt("age?")
-let message = (age<3) ? "hi Baby!" :
-    (age<18) ? "Hi Kiddo":
-    (age<100) ? "Hi Adult":
-    "Ohh you are Centurion";
-alert(message)
-let dateofBirth = 2021-age
-if (age <18) {
-    alert("Hi"+ name + "." + `your age is ${age}.`+ `You were born in ${dateofBirth}` +
-     "you are below 18 so you cannot access the website");
+let answer = prompt("What is official name of Java Scrip?")
+if (answer == 'ECMAScript') {
+    alert(`Hi ${name}. You are correct`);
 }
-else {alert("Hi"+ name + "." + `your age is ${age}.`+ `You were born in ${dateofBirth}`+"you are above 18, so access the website");}
-
-
-
-
-
+    else {alert(`Hi ${name}. You are incorrect. Its ECMAScript`);
+}
