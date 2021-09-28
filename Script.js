48753ce1149c781aecf3835a8db02d8394807d81
PrashantSomaniIIMI <90964968+PrashantSomaniIIMI@users.noreply.github.com>
2021-09-28 11:43:01 +0530

    Starting with JS, using prompt and alert

---
+++ Script.js
@@ -0,0 +1,10 @@
+let name = prompt("what is your name")
+let age = prompt("how old are you")
+let isBoss = confirm("are you above 18 years")
+let dateofBirth = 2021-age
+alert("Hi"+ name + "." + `your age is ${age}.`+ `You were born in ${dateofBirth}`);
+
+
+
+
+
