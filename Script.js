e1422a1e119e3fe3e63a6464ab7e7b9943a71c10
PrashantSomaniIIMI <90964968+PrashantSomaniIIMI@users.noreply.github.com>
2021-09-28 12:07:02 +0530

    If else and ? variable use

--- Script.js
+++ Script.js
@@ -1,5 +1,10 @@
 let name = prompt("what is your name")
-let age = prompt("how old are you")
+let age = prompt("age?")
+let message = (age<3) ? "hi Baby!" :
+    (age<18) ? "Hi Kiddo":
+    (age<100) ? "Hi Adult":
+    "Ohh you are Centurion";
+alert(message)
 let dateofBirth = 2021-age
 if (age <18) {
     alert("Hi"+ name + "." + `your age is ${age}.`+ `You were born in ${dateofBirth}` +
