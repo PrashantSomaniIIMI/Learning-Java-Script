7d45ca8521dd5fe141d0c2775c3d5e61cf752e26
PrashantSomaniIIMI <90964968+PrashantSomaniIIMI@users.noreply.github.com>
2021-09-28 13:59:48 +0530

    using and to check age between 18 and 100

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 let name = prompt("what is your name")
 let age = prompt("What is your age")
-if (age<18||age>100) {
-    alert(`Hi ${name}, you are too young or too old to try this game`)
-    }
-    else {alert('Hi ${name}, you are ready to try the game')}
+if (age>=18 && age<=100) {
+    alert(`Hi ${name}, you can try this game`)
+    } else {alert("no you cant")}
+   
